8385078a50550878d36a7b88d0648fe7bc7d3a2a
Ammar Rexhaj <167920633+bandyburrito@users.noreply.github.com>
2024-07-10 02:50:15 -0700

    Version 1

---
+++ detyra.py
@@ -0,0 +1,32 @@
+x = input("Sa eshte shuma e pikeve qe keni marr: ")
+if (int(x) < 29):
+    print("Nota juaj eshte 1")
+
+elif (int(x) < 49):
+    print("Nota juaj eshte 2")
+
+elif (int(x) < 69):
+    print("Nota juaj eshte 3")
+
+elif (int(x) < 89):
+    print("Nota juaj eshte 4")
+
+elif (int(x) > 89):
+    print("Nota juaj eshte 5")
+
+elif (int(x) > 100):
+    print("Shuma pikeve duhet te jet derin ne 100 pike")
+
+elif (int(x) < 0):
+    print("Shuma pikeve duhet te jet derin ne 100 pike")
+
+
+
+
+
+
+ 
+
+
+
+
